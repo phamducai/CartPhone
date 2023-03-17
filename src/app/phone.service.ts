4473607c8aa477f56c4be9a76282acf2dfe97fbf
phamducai <ai.phamphamducai@hcmut.edu.vn>
2023-03-17 17:14:47 +0700

    haha

--- src/app/phone.service.ts
+++ src/app/phone.service.ts
@@ -2,18 +2,17 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { of } from 'rxjs';
 import { Phone } from 'src/models/phone';
-import { fakePhones } from './fake-movieshopping';
+import { fakePhones } from './fake-phonedata';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PhoneService {
-
-  constructor() { }
-  getPhones(): Observable<Phone[]>{
-    return of(fakePhones)
+  constructor() {}
+  getPhones(): Observable<Phone[]> {
+    return of(fakePhones);
   }
-  getPhoneById(id: string|null): Observable<Phone|undefined> {
-    const idNumber =Number(id)
-    return of(fakePhones.find(item=>item.id===idNumber))}
-
+  getPhoneById(id: string | null): Observable<Phone | undefined> {
+    const idNumber = Number(id);
+    return of(fakePhones.find((item) => item.id === idNumber));
+  }
 }
